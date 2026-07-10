5b0e4c2b5811c0760fff2fb118f6470e672edb8e
Diane Uwamariya <dianahporter@gmail.com>
2026-07-10 14:19:14 +0200

    feat: created user service and reposiroty

---
+++ user-service/src/main/java/com/ecommerce/user_service/service/UserService.java
@@ -0,0 +1,46 @@
+package com.ecommerce.user_service.service;
+
+import com.ecommerce.user_service.entity.User;
+import com.ecommerce.user_service.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    public User updateUser(Long id, User updatedUser) {
+
+        User existingUser = getUserById(id);
+
+        existingUser.setFirstName(updatedUser.getFirstName());
+        existingUser.setLastName(updatedUser.getLastName());
+        existingUser.setEmail(updatedUser.getEmail());
+        existingUser.setPassword(updatedUser.getPassword());
+
+        return userRepository.save(existingUser);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
