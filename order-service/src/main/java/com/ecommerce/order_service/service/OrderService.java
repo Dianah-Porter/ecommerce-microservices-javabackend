db90d70fcb635cc5793d4ed14155e93b030188d6
Diane Uwamariya <dianahporter@gmail.com>
2026-07-10 18:55:54 +0200

    feat: order service

---
+++ order-service/src/main/java/com/ecommerce/order_service/service/OrderService.java
@@ -0,0 +1,59 @@
+package com.ecommerce.order_service.service;
+
+import com.ecommerce.order_service.entity.Order;
+import com.ecommerce.order_service.repository.OrderRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Service
+public class OrderService {
+
+    private final OrderRepository repository;
+    private final RestTemplate restTemplate;
+
+    public OrderService(OrderRepository repository,
+                        RestTemplate restTemplate) {
+        this.repository = repository;
+        this.restTemplate = restTemplate;
+    }
+
+    public Order create(Order order) {
+
+        restTemplate.getForObject(
+                "http://localhost:8082/products/" + order.getProductId(),
+                Object.class);
+
+        restTemplate.getForObject(
+                "http://localhost:8083/inventory/" + order.getProductId(),
+                Object.class);
+
+        order.setStatus("CREATED");
+
+        return repository.save(order);
+    }
+
+    public List<Order> getAll() {
+        return repository.findAll();
+    }
+
+    public Order getById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() ->
+                        new RuntimeException("Order not found"));
+    }
+
+    public Order update(Long id, Order updated) {
+
+        Order order = getById(id);
+
+        order.setQuantity(updated.getQuantity());
+
+        return repository.save(order);
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+}
